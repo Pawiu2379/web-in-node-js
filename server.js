13ab67824c54b55c66de937853cc127887ee5d18
Pawiu__ <pawelkoc681@gmail.com>
2023-02-13 00:44:31 +0100

    add routes

--- server.js
+++ server.js
@@ -11,3 +11,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// ==============================================================
+const aboutRouter = require('./routes/about')
+const featuresRouter = require('./routes/features')
+const panelRouter = require('./routes/panel')
+const tutorialsRouter = require('./routes/tutorials')
+
+app.use('/about', aboutRouter)
+app.use('/features',featuresRouter)
+app.use('/panel', panelRouter)
+app.use("/tutorials", tutorialsRouter)
